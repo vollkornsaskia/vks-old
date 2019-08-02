f2f22e5b2dfce286644e14f65ec99f9238f16a4a
Saskia Bayreuther <saskia@byrthr.com>
2019-08-02 15:59:05 +0200

    Nikola auto commit.
    Nikola version: 8.0.2

--- conf.py
+++ conf.py
@@ -249,11 +249,11 @@ TIMEZONE = "Europe/Berlin"
 # Date format used to display post dates. (translatable)
 # Used by babel.dates, CLDR style: http://cldr.unicode.org/translation/date-time
 # You can also use 'full', 'long', 'medium', or 'short'
-# DATE_FORMAT = 'YYYY-MM-dd HH:mm'
+DATE_FORMAT = 'dd.MM.YYYY'
 
 # Date format used to display post dates, if local dates are used. (translatable)
 # Used by moment.js: https://momentjs.com/docs/#/displaying/format/
-# JS_DATE_FORMAT = 'YYYY-MM-DD HH:mm'
+JS_DATE_FORMAT = 'dd.MM.YYYY'
 
 # Date fanciness.
 #
